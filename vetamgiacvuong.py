03dff0b95b8a1f8dfd1d04ad19519ebd098785a2
unknown <geodev2003@gmail.com>
2026-07-13 07:56:33 +0700

    Ve tam giac vuong

--- vetamgiacvuong.py
+++ vetamgiacvuong.py
@@ -26,3 +26,24 @@ for i in range(h, 0, - 1):
 # Hàm rjust() - Right Justify - Căn lề phải
 for i in range(0, h):
     print(("*" * i).rjust(h))
+    
+
+# Hàm ljust() - Left Justify - Căn lề trái
+for i in range(0, h):
+    print(("*" * i).ljust(h))
+    
+
+# Hàm center() - Center - Căn giữa
+#        *    
+#       ***   
+#      *****  
+#     ******* 
+#    *********
+
+chieu_rong_day = 2 * h - 1
+
+for i in range(1, h + 1):
+    so_sao = 2 * i - 1
+    chuoi_sao = "*" * so_sao
+    
+    print(chuoi_sao.center(chieu_rong_day))
